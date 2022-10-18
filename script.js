1a3d88f391a3d7694d0851028fd3386f085f1f37
aismeg <iaskja20@gmai.com>
2022-10-18 15:53:32 +0300

    beta

--- script.js
+++ script.js
@@ -2,37 +2,40 @@
 
 let random = Math.ceil(Math.random() * 100);
 
+
 function process() {
 
     let ask = +prompt('Угадай число от 1 до 100');
 
     function counter(num) {
-        switch (num === ask) {
+
+        switch (true) {
             case num > random:
                 alert('Загаданное число меньше');
+                console.log(random);
                 break;
             case num < random:
                 alert('Загаданное число больше');
+                console.log(random);
                 break;
             case num === random:
                 alert('Поздравляю, Вы угадали!!!');
+                console.log(random);
                 break;
             default:
                 alert('Введи число!');
                 process();
         }
 
-        let confirmThis = confirm('Ввести новый вариант?');
-
-        switch (confirmThis) {
-            case true:
-                process();
-                break;
-            default: alert('Игра окончена');
-        }
-
+        process();
     }
-    counter(ask);
+
+    switch (true) {
+        case true:
+            counter(ask);
+            break;
+        default: alert('Игра окончена');
+    }
 
 }
 process();
